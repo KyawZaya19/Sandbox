1f0a018b7afab73ed132ca7e661ec64a22ba32bb
Kyaw Zaya <kgkgzenzen1989@gmail.com>
2022-03-30 11:44:36 +0630

    Add docstrings.

--- password_check.py
+++ password_check.py
@@ -1,7 +1,11 @@
+"""
+A simple program for password check
+"""
+
 MIN_LENGTH = 6
-password = input("Type a passsword : ")
+password = input("Type a password : ")
 while len(password) < MIN_LENGTH:
-	print("Your password is too short")
-	password = input("Type a passsword : ")
+    print("Your password is too short")
+    password = input("Type a password : ")
 for x in password:
-	print("*",end="")
+    print("*", end="")
